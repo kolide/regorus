46b55a4db68689fd2df8d7b89ef60e8b69fdb38a
Chris Born <chris.born@agilebits.com>
2025-08-14 13:34:25 -0700

    More work on the build process and make the loader more resilient

--- bindings/ruby/ext/regorusrb/extconf.rb
+++ bindings/ruby/ext/regorusrb/extconf.rb
@@ -6,27 +6,33 @@ require "rb_sys/mkmf"
 
 cpu = RbConfig::CONFIG["host_cpu"]
 os  = RbConfig::CONFIG["host_os"]
+musl = os =~ /musl|alpine/
 arch =
   if os =~ /linux/
     if cpu =~ /aarch64|arm64/
-      "aarch64-linux"
+      musl ? "aarch64-linux-musl" : "aarch64-linux"
     elsif cpu =~ /x86_64|amd64/
-      "x86_64-linux"
+      musl ? "x86_64-linux-musl" : "x86_64-linux"
     end
   end
 
-DLEXT = RbConfig::CONFIG["DLEXT"]            # "so" on Linux
+DLEXT   = RbConfig::CONFIG["DLEXT"]
 dest_dir = File.expand_path("../../lib/regorus", __dir__)
 dest_so  = File.join(dest_dir, "regorusrb.#{DLEXT}")
 prebuilt = arch && File.expand_path("../../vendor/native/#{arch}/regorusrb.#{DLEXT}", __dir__)
 
 if prebuilt && File.exist?(prebuilt)
-  # Use prebuilt, no-op Makefile so bundler is happy.
-  FileUtils.mkdir_p(dest_dir)
-  FileUtils.cp(prebuilt, dest_so)
-  File.write("Makefile", "all:\ninstall:\n\t@true\nclean:\n\t@true\n")
+  # Defer the copy to `make install` so it runs in all fresh-install flows
+  File.write("Makefile", <<~MK)
+        all:
+        install:
+    \tmkdir -p #{dest_dir}
+    \tcp #{prebuilt} #{dest_so}
+        clean:
+    \t@true
+  MK
 else
-  # Fallback: compile (dev/mac, or if a prebuilt is missing)
+  # Fallback: compile (e.g., dev on macOS, or if a prebuilt is missing)
   create_rust_makefile("regorus/regorusrb") do |r|
     r.auto_install_rust_toolchain = true
   end
